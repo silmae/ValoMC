06fa91b4366207c270c4ccbbe0e80f133ff4c391
kiauriih <kimmo.a.riihiaho@jyu.fi>
2021-02-16 16:27:05 +0200

    Plot boundary exitance

--- examples/leaf2d.m
+++ examples/leaf2d.m
@@ -3,7 +3,7 @@
 % Create a rectangular mesh
 xsize =  100;    % width of the region [mm]
 ysize =  10;    % height of the region [mm]
-dh = 0.1;       % discretisation size [mm]
+dh = 0.2;       % discretisation size [mm]
 x_half = xsize / 2;
 y_half = ysize / 2;
 vmcmesh = createRectangularMesh(xsize, ysize, dh);
@@ -14,17 +14,17 @@ vmcmedium = createMedium(vmcmesh);
 vmcmedium.absorption_coefficient = 0.003;     % absorption coefficient [1/mm]
 vmcmedium.scattering_coefficient = 0.001;      % scattering coefficient [1/mm]
 vmcmedium.scattering_anisotropy = 0.0;       % scattering anisotropy parameter [unitless] 
-vmcmedium.refractive_index = 1.0;            % refractive index [unitless]
+vmcmedium.refractive_index = 1.0003;            % refractive index [unitless]
 
 leaf_width = 50.0;
-leaf_height = 0.4;
+leaf_height = 0.8;
 rectangle_position=[0 0];
 leaf_elements= findElements(vmcmesh, 'rectangle', rectangle_position, leaf_width, leaf_height);
 % Set the optical coefficients inside the leaf
-vmcmedium.absorption_coefficient(leaf_elements) = 0.3; % absorption
-vmcmedium.scattering_coefficient(leaf_elements) = 0.1;   % scattering
-vmcmedium.scattering_anisotropy(leaf_elements) = 0.9;   % g
-vmcmedium.refractive_index(leaf_elements) = 1.4;           % index of refraction
+% vmcmedium.absorption_coefficient(leaf_elements) = 0.003; % absorption
+vmcmedium.scattering_coefficient(leaf_elements) = 0.001;   % scattering
+% vmcmedium.scattering_anisotropy(leaf_elements) = 0.0;   % g
+% vmcmedium.refractive_index(leaf_elements) = 1.0003;           % index of refraction
 
 %% Set up the boundary and create light sources
 %
@@ -57,17 +57,31 @@ solution = ValoMC(vmcmesh, vmcmedium, vmcboundary);
 
 %% Plot the solution
 hold on;
+plot3d = true;
 
 patch('Faces',vmcmesh.H,'Vertices',vmcmesh.r,'FaceVertexCData', solution.element_fluence, 'FaceColor', 'flat','EdgeColor','none');
 xlabel('[mm]');
 ylabel('[mm]');
-
-% text(-xsize/2+0.4, 0, 'direct lightsource');
-% text(0, -ysize/2+0.4, 'gaussian tilted by 22.5 deg', 'HorizontalAlignment', 'center');
-% text(xsize/2-0.4, 0, 'cosinic', 'HorizontalAlignment', 'right');
-% text(0, ysize/2-0.4, 'isotropic', 'HorizontalAlignment', 'center');
-
 c = colorbar;                   
 c.Label.String = 'Fluence [J/mm^2]';
+
+if plot3d
+    % The boundary solution is given as a constant fluence and extitance values
+    % at each boundary element.
+
+    % To plot the boundary fluence with plot3, an average coordinate of each
+    % boundary element is calculated
+
+    avgr = (vmcmesh.r(vmcmesh.BH(:, 1),:) + vmcmesh.r(vmcmesh.BH(:, 2),:))/2;
+    h = plot3(avgr(:,1), avgr(:,2), (solution.boundary_exitance), 'b','LineWidth',1.5);
+    legend(h,{'Boundary exitance'});
+
+    % tilt the view
+    az = -54;
+    el = 34;
+    view([az, el]);
+    zlabel('Boundary value [(J/mm^2)]');
+end
+
 hold off
 
